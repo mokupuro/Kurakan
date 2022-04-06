82f180436aeb889f402d51006dcf83607ba00419
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-06 23:45:58 +0900

    feat: circle image model association

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -1,4 +1,7 @@
 class Circle < ApplicationRecord
   has_one :circle_time, dependent: :destroy
+  has_one :circle_image, dependent: :destroy
+  has_one :image, through: :circle_image, class_name: 'Image', source: :image
+
   has_many :circle_days, dependent: :destroy
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   has_one_attached :image
 
-  has_one :circle_image
+  has_one :circle_image, dependent: :destroy
 end
