9d8c565e10129dc1e39226b352938db7fd597e83
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-07 11:41:10 +0900

    feat: validate for image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,9 @@
 class Image < ApplicationRecord
   has_one_attached :image
-
   has_one :circle_image, dependent: :destroy
+
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                    message: "must be a valid image format" },
+                    size: { less_than: 5.megabytes,
+                            message: "should be less than 5MB" }
 end
