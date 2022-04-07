64dbb14a40d7aace420b187df18ef334b6a4b84b
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-07 14:02:50 +0900

    feat: method to use in the circle view

--- app/models/circle.rb
+++ app/models/circle.rb
@@ -4,4 +4,8 @@ class Circle < ApplicationRecord
   has_one :image, through: :circle_image, class_name: 'Image', source: :image
 
   has_many :circle_days, dependent: :destroy
+
+  def active_days
+    circle_days.map { |circle_day| circle_day.active_day}
+  end
 end
--- app/models/circle_time.rb
+++ app/models/circle_time.rb
@@ -1,3 +1,11 @@
 class CircleTime < ApplicationRecord
   belongs_to :circle
+
+  def display_start_time
+    self.active_start_time.strftime("%I:%M")
+  end
+
+  def display_end_time
+    self.active_end_time.strftime("%I:%M")
+  end
 end
