d5c517d2a921831f336f527c2a762a831d9b741e
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-14 21:45:57 +0900

    feat: create and update to circle in circle controller

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -10,20 +10,42 @@ class CirclesController < ApplicationController
 
   def new
     @circle = Circle.new
+    @circle.build_circle_time
+    @day_of_weeks = DayOfWeek.all
   end
 
   def edit
   end
 
   def create
+    @circle = Circle.new(circle_params)
+    if @circle.save
+      flash[:success] = "作成しました"
+      redirect_to @circle
+    else
+      flash[:danger] = "作成に失敗しました"
+      render 'new'
+    end
+  end
+
+
+  def update
+    if @circle.update(circle_params)
+      flash[:success] = "更新しました"
+      redirect_to @circle
+    else
+      flash[:danger] = "更新に失敗しました"
+      render 'edit'
+    end
   end
 
   private
     def set_circle
       @circle = Circle.find(params[:id])
+      @day_of_weeks = DayOfWeek.all
     end
 
     def circle_params
-      params.require(:circle).permit(:name, :description, :active_place, :number_of_people, :link)
+      params.require(:circle).permit(:name, :description, :active_place, :number_of_people, :link, day_of_week_ids: [], circle_time_attributes: [:active_start_time, :active_end_time])
     end
 end
