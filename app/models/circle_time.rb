b79daaac21e71d82f7e4ed4ac5385978d370165b
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-14 20:02:43 +0900

    modify: add ボッチ演算子

--- app/models/circle_time.rb
+++ app/models/circle_time.rb
@@ -2,10 +2,10 @@ class CircleTime < ApplicationRecord
   belongs_to :circle
 
   def display_start_time
-    self.active_start_time.strftime("%I:%M")
+    self.active_start_time&.strftime("%I:%M")
   end
 
   def display_end_time
-    self.active_end_time.strftime("%I:%M")
+    self.active_end_time&.strftime("%I:%M")
   end
 end
