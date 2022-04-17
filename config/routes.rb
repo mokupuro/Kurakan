cdb84d001eb626c7eb83e26f7f840f18c384e816
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-17 17:28:36 +0900

    feat: admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#index'
   get "circles/:id/favorites" => "circles#favorites"
 
+  devise_for :admin_users, controller: {
+    sessions: 'admin_users/sessions',
+    passwords: 'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  resources :circles
   resources :circles do
     resources :favorites, only: [:create, :destroy]
   end
 
-  resources :circles
   resources :users
 
   # FIXME 下記を追加するとactive Storageの画像のリンクも含まれるため画像が表示されない
