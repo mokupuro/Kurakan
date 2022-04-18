ede559b3293287c88922cfe24d980c1f9b78c183
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-18 16:56:07 +0900

    fix: delete forigenkey error for assection

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ApplicationRecord
   has_one_attached :image
   has_one :circle_image, dependent: :destroy
+  has_one :circle_post_image, dependent: :destroy
 
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
                                     message: "must be a valid image format" },
