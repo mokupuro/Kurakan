40c6072bf22f2fbf6c40bc1e5008ce7983a2d0cf
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-19 11:30:06 +0900

    feat: category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,17 @@ CircleAccount.destroy_all
 Circle.destroy_all
 User.destroy_all
 DayOfWeek.destroy_all
+Category.destroy_all
 
 %w[月 火 水 木 金 土 日].each do |day_of_week|
   DayOfWeek.create!(day_name: day_of_week)
 end
+
+Category.create(name: '強化クラブ' )
+Category.create(name: '準強化クラブ')
+Category.create(name: '部')
+Category.create(name: '同好会')
+Category.create(name: '愛好会')
 
 circle = create(:circle)
 user = create(:user)
